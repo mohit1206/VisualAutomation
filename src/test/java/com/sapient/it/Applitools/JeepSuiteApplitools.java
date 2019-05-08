aa5e8a2272397d03142062495067b805461722a4
mohit1206 <30656997+mohit1206@users.noreply.github.com>
2019-05-08 09:38:09 +0530

    Update JeepSuiteApplitools.java

--- src/test/java/com/sapient/it/Applitools/JeepSuiteApplitools.java
+++ src/test/java/com/sapient/it/Applitools/JeepSuiteApplitools.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -19,47 +20,55 @@ import au.com.bytecode.opencsv.CSVReader;
 
 public class JeepSuiteApplitools {
 	Eyes eyes;
-	int count =0;
+	int count = 0;
 	String[] str = null;
+
 	@Test
-	public void suite() throws IOException, InterruptedException{
+	public void suite() throws IOException, InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		CSVReader reader = new CSVReader(new FileReader("./Resources/Feature.csv"));
-         List<String[]> list=reader.readAll();
-         System.out.println("Total rows which we have is "+list.size());
-         Iterator<String[]> iterator= list.iterator();
-         Eyes eyes= new Eyes();
-         eyes.setApiKey("a39B91bM19NMvdkaWFMecVDbotqn110HR1P6nP8P8amOc110");
-         eyes.setForceFullPageScreenshot(true);
- 		 eyes.setStitchMode(StitchMode.CSS);
-   	   	 eyes.open(driver, "jeepusa4","windowJeepSuite4", new RectangleSize(900, 600));
-        while(iterator.hasNext()){
-              str=iterator.next();
-        for(int i=0;i<str.length;i++) {
-      	  count++;
-      	  driver.get(str[i]);
-      	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-      	  Thread.sleep(2000);
-      	  try{
-      	  if(driver.findElement(By.xpath("//a[@data-lid='go-to-jeep-us']")).isDisplayed()){
-      		driver.findElement(By.xpath("//a[@data-lid='go-to-jeep-us']")).click();
-      		Thread.sleep(2000);
-      	  }
-      	  }catch(Exception e){
-      	  }
-      	  eyes.checkWindow("jeepBHPVLPPageSuite"+count);
-        }
-        }
-        try{
-      	  eyes.close();
-          } catch(Exception e){
-    	  eyes.abortIfNotClosed();
-          }
-      driver.close();
-    	System.exit(0);
+		List<String[]> list = reader.readAll();
+		System.out.println("Total rows which we have is " + list.size());
+		Iterator<String[]> iterator = list.iterator();
+		Eyes eyes = new Eyes();
+		eyes.setApiKey("a39B91bM19NMvdkaWFMecVDbotqn110HR1P6nP8P8amOc110");
+		eyes.setForceFullPageScreenshot(true);
+		eyes.setStitchMode(StitchMode.CSS);
+		eyes.open(driver, "jeep_usa", "JeepSuiteForWindows", new RectangleSize(900, 600));
+		while (iterator.hasNext()) {
+			str = iterator.next();
+			for (int i = 0; i < str.length; i++) {
+				count++;
+				driver.get(str[i]);
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				Thread.sleep(2000);
+				try {
+					if (driver.findElement(By.xpath("//a[@data-lid='go-to-jeep-us']")).isDisplayed()) {
+						driver.findElement(By.xpath("//a[@data-lid='go-to-jeep-us']")).click();
+						Thread.sleep(2000);
+					}
+				} catch (Exception e) {
+				}
+				for (int second = 0;; second++) {
+					if (second >= 25) {
+						break;
+					}
+					((JavascriptExecutor) driver).executeScript("window.scrollBy(0,400)", "");
+					Thread.sleep(1000);
+				}
+				eyes.checkWindow("jeepBHPVLPPages" + count);
+			}
+		}
+		try {
+			eyes.close();
+		} catch (Exception e) {
+			eyes.abortIfNotClosed();
+		}
+		driver.close();
+		System.exit(0);
 
 	}
-        
+
 }
